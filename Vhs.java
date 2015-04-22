351a83777699e43de739162691928cc672bb3801
phlyper <phlyper@live.fr>
2015-04-22 16:14:07 +0100

    Update Vhs.java

--- Vhs.java
+++ Vhs.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tpyak;
 
 /**
@@ -35,9 +30,4 @@ public class Vhs extends Article {
 		return 1.5 + (getFilm() != null ? getFilm().isNouveaute() ? 0.5 : 0 : 0);
 	}
 
-	@Override
-	public void finalize() throws Throwable {
-		super.finalize();
-		System.out.println(this.getClass().getName()+" Objet nettoyé de la mémoire");
-	}
 }
