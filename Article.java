62694090ccc0a1b31a4b6790857e28c283b36219
phlyper <phlyper@live.fr>
2015-04-22 16:11:15 +0100

    Update Article.java

--- Article.java
+++ Article.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tpyak;
 
 /**
@@ -60,9 +55,4 @@ public abstract class Article {
 		this.noInventaire = noInventaire;
 	}
 
-	@Override
-	public void finalize() throws Throwable {
-		super.finalize();
-		System.out.println(this.getClass().getName()+" Objet nettoyé de la mémoire");
-	}
 }
