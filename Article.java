6a2302d1892d116cd0bf765bc2efb917b8f63e95
phlyper <phlyper@live.fr>
2015-04-22 16:08:39 +0100

    Create Article.java

---
+++ Article.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tpyak;
+
+/**
+ *
+ * @author Atef
+ */
+public abstract class Article {
+
+	private static int noArticle = 1;
+	private int noInventaire;
+	private Film film = null;
+	private Adherent emprunteur = null;
+
+	protected Article(Film film) {
+		this.film = film;
+		noInventaire = noArticle;
+		noArticle++;
+	}
+
+	public Film getFilm() {
+		return film;
+	}
+
+	public Adherent getEmprunteur() {
+		return emprunteur;
+	}
+
+	public void setEmprunteur(Adherent adherent) {
+		this.emprunteur = adherent;
+	}
+
+	public String getDescription(boolean complete) {
+		String s = "";
+		s += String.format("Numéro d’inventaire : %d\n", noInventaire);
+		if (complete) {
+			s += String.format("Titre du film : %s\n", film != null ? film.getTitre() : "");
+			s += String.format("Réalisateur : %s %s\n", film != null ? film.getRealisateur().getNom() : "", film != null ? film.getRealisateur().getPrenom() : "");
+		}
+		s += String.format("Nouveauté : %s\n", film.isNouveaute() ? "oui" : "non");
+		s += String.format("%s\n", emprunteur == null ? "Disponible" : String.format("Emprunté par le membre n° : %d", emprunteur.getNoAdherent()));
+		return s;
+	}
+
+	public abstract double tarifLocation();
+
+	public void setNoArticle(int noArticle) {
+		this.noArticle = noArticle;
+	}
+
+	public int getNumeroInventaire() {
+		return noInventaire;
+	}
+
+	public void setNoInventaire(int noInventaire) {
+		this.noInventaire = noInventaire;
+	}
+
+	@Override
+	public void finalize() throws Throwable {
+		super.finalize();
+		System.out.println(this.getClass().getName()+" Objet nettoyé de la mémoire");
+	}
+}
